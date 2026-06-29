825a8525ba4153ee927dcd0827c5b6481e1927c0
RiyaGoel <riyagoel314@gmail.com>
2026-06-29 15:06:47 +0530

    updated

--- ARRAYS/mergeIntervals.cpp
+++ ARRAYS/mergeIntervals.cpp
@@ -8,21 +8,18 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-
-        sort(intervals.begin(), intervals.end());
-
-        vector<vector<int>> ans;
-
-        for (auto &it : intervals) {
-
-            if (ans.empty() || it[0] > ans.back()[1]) {
-                ans.push_back(it);
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>result;
+        if(intervals.empty()) return {};
+            result.push_back({intervals[0][0],intervals[0][1]});
+        for(int i=1; i<intervals.size();i++) {
+            if(result.back()[1]>=intervals[i][0]) {
+                result.back()[1]=max(result.back()[1],intervals[i][1]);
             }
             else {
-                ans.back()[1] = max(ans.back()[1], it[1]);
+                result.push_back({intervals[i][0],intervals[i][1]});
             }
         }
-
-        return ans;
+        return result;
     }
 };
